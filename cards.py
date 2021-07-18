ee4502316a719d65131436415c2aab3062708523
caseyjett <crjett01@gmail.com>
2021-07-18 11:18:19 -0400

    Finish game for player and 2 computers

--- cards.py
+++ cards.py
@@ -31,19 +31,17 @@ rooms = [
     "Study"
 ]
 
-the_murder = ['Mrs. Peacock', 'Hall', 'Lead pipe']
-# def random_murder():
-#     murderer = random.choice(characters)
-#     murder_location = random.choice(rooms)
-#     murder_weapon = random.choice(weapons)
+the_murder = []
+def random_murder():
+    murderer = random.choice(characters)
+    murder_location = random.choice(rooms)
+    murder_weapon = random.choice(weapons)
 
-#     the_murder.append(murderer)
-#     the_murder.append(murder_location)
-#     the_murder.append(murder_weapon)
+    the_murder.append(murderer)
+    the_murder.append(murder_location)
+    the_murder.append(murder_weapon)
 
-def determine_game_size():
-    number_of_players = int(input("How many players would you like to compete against? Pick a number between 2 and 5: "))
-    return number_of_players
+
 
 cards = []
 cards.extend(characters)
@@ -56,7 +54,13 @@ cards.remove(the_murder[2])
 
 random.shuffle(cards)
 
-#This game size DOES NOT account for the player, only the number of other players
+
+
+def determine_game_size():
+    number_of_players = int(input("How many players would you like to compete against? Pick a number between 2 and 5: "))
+    return number_of_players
+
+# This game size DOES NOT account for the player, only the number of other players
 game_size = determine_game_size()
 
 
@@ -73,15 +77,6 @@ for row, column in my_dataframe:
 
 
 
-    
-# THIS WILL DIVIDE THE CARDS LIST INTO SUBLISTS 
-# def split(a, n):
-#     k, m = divmod(len(a), n)
-#     return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
-# smaller_deck = list(split(cards, game_size + 1))
-# player_deck = list(smaller_deck[0])
-
-# print("\nYou're clues are:", player_deck, "\n")
 
 
--- play_clue.py
+++ play_clue.py
@@ -41,10 +41,41 @@ def random_murder():
     the_murder.append(murder_location)
     the_murder.append(murder_weapon)
 
-#DETERMINE NUMBER OF PLAYERS
-def determine_game_size():
-    number_of_players = int(input("How many players would you like to compete against? Pick a number between 2 and 5: "))
-    return number_of_players
+# #DETERMINE NUMBER OF PLAYERS *This would be used if I can figure out a way to sort cards with a varying number of players
+# def determine_game_size():
+#     number_of_players = int(input("How many players would you like to compete against? Pick a number between 2 and 5: "))
+#     return number_of_players
+
+#CALLING THE ACTION
+random_murder()
+
+#BUILDING CARD DECKS
+cards = []
+cards.extend(characters)
+cards.extend(weapons)
+cards.extend(rooms)
+
+cards.remove(the_murder[0])
+cards.remove(the_murder[1])
+cards.remove(the_murder[2])
+
+random.shuffle(cards)
+
+#SORT CARDS and assign to the 3 players
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+smaller_deck = list(split(cards, 3))
+
+player_cards = smaller_deck[0]
+computer_1 = smaller_deck[1]
+computer_2 = smaller_deck[2]
+
+# These will be the clues that the player has seen
+seen_clues = []
+for i in player_cards:
+    seen_clues.append(i)
 
 #BEGINNING OF CLASSES
 class Player: 
@@ -59,30 +90,47 @@ class Player:
             print(f"---{idx}) {character}")
         chosen_character = int(input("\nEnter a number: "))
         play_as = characters[chosen_character]
-        print("You are now " + play_as + "\n")
+        print(f"You are now {play_as} \n")
         self.char = characters[chosen_character] 
-        # find the chosen character and print it like "Okay Mr. Green welcome to the ... and explain rules of the game. Also remove player from the list of people that can be asked"
-        #     pass
+        print(f"\n**{self.char}, today there has been a murder at the Tudor Mansion and it is your job to solve it. You, Detective Gordon, and Sherlock each have six clues. Those clues tell you people that are NOT the murderer, rooms that are NOT the murder location, and items that are NOT the murder weapon. You will navigate room to room guessing what may have happened in that room. When you guess, Detective Gordon or Sherlock will share their clues if they have any. Once you have determined the murder details, you will make an accusation. You only get one chance to make an accusation so make sure that you are positive! You begin in the Billiard Room and can only move into rooms that are north, south, east or west. Rooms at the 4 corners of the mansion have secret tunnels connecting them to the room diagonal from them. You can check out the map anytime you want to. \n\nHere are your clues: {player_cards}\n")
+
 
     def next_room(self):
         current = self.current_room
-        print("Which room would you like to go to next,", self.char,"?")
+        print(f"Which room would you like to go to next, {self.char}?")
         for idx, room in enumerate(current.connections):
             print(f'---{idx}) {room.name}')
         chosen_room = int(input("\nEnter a number: "))
         self.current_room = current.connections[chosen_room]
-        print("You are now in the ", self.current_room.name,"\n") 
+        print(f"You are now in the {self.current_room.name}.\n") 
 
     def guess(self):
         print("\nGuess the killer:")
         for idx, character in enumerate(characters):
             print(f"---{idx}) {character}")
         murderer_guess = int(input(" "))
+        print("\nGuess the weapon:")
         for idx, weapon in enumerate(weapons):
             print(f"---{idx}) {weapon}")
         weapon_guess = int(input(" "))
-        print("You are guessing that: ", characters[murderer_guess], "killed Mr. Body with the", weapons[weapon_guess], "in the", self.current_room.name, "\n")
+        print(f"You are guessing that: {characters[murderer_guess]} killed Mr. Body with the {weapons[weapon_guess]} in the {self.current_room.name}.\n")
         #Work the cards with these guesses somehow
+        guess = 0
+        for i in range(len(computer_1)):
+            if computer_1[i] == characters[murderer_guess] or computer_1[i] == weapons[weapon_guess] or computer_1[i] == self.current_room.name:
+                # Need a way to only show one card not all of them
+                print(f"Detective Gordon knows that it's not: {computer_1[i]} \n")
+                seen_clues.append(f'{computer_1[i]} from Detective Gordon')
+                guess += 1
+        if guess < 1: 
+            for i in range(len(computer_1)):
+                if computer_2[i] == characters[murderer_guess] or computer_2[i] == weapons[weapon_guess] or computer_2[i] == self.current_room.name:
+                    # Need a way to only show one card not all of them
+                    print(f"Sherlock knows that it's not: {computer_2[i]} \n")
+                else:
+                    print("Detective Gordon and Sherlock don't have any clues to share.")
+    
+            
 
     def accusation(self):
         warning = input("Warning: An accusation will end the game. Are you confident enough to proceed? Y/N  ")
@@ -91,25 +139,28 @@ class Player:
             for idx, character in enumerate(characters):
                 print(f"---{idx}) {character}")
             murderer_accusation = int(input("Enter the number corresponding with the murderer: "))
+            print("\n")
             for idx, weapon in enumerate(weapons):
                 print(f"---{idx}) {weapon}")
             weapon_accusation = int(input("Enter the number corresponding with the murder weapon: "))
+            print("\n")
             for idx, room in enumerate(rooms):
                 print(f"---{idx}) {room}")
             room_accusation = int(input("Enter the number corresponding with the room where the murder took place: "))
+            print("\n")
             #Print characters, weapons, and rooms
-            print("--You believe that ", characters[murderer_accusation], " used the ", weapons[weapon_accusation], " in the ", rooms[room_accusation],"!!--")
+            print(f"--You believe that {characters[murderer_accusation]} used the {weapons[weapon_accusation]} in the {rooms[room_accusation]}!!--")
             final_accusation = input("Is this your final accusation? Y/N  ").upper()
             if final_accusation == "Y":
-                #Logic to match murder with accusation
-                pass
+                if the_murder[0] == characters[murderer_accusation] and the_murder[1] == rooms[room_accusation] and the_murder[2] == weapons[weapon_accusation]:
+                    print(f"\nCongratulation {self.char} you have solved Mr. Body's murder!!!\n\n")
+                else:
+                    print(f"\nToday you failed Mr. Body. Here is what actaully happened: {the_murder[0]} killed Mr. Body in the {the_murder[1]} with the {the_murder[2]}.\n\n")
             elif final_accusation == "N":
                 self.accusation()
             else:
                 print("That was not a valid response. Try again\n")
                 self.accusation()
-            
-
         else: 
             #Exit the accusation
             pass
@@ -180,15 +231,9 @@ def in_play():
     player.pick_player()
 
 
-
-    #Eventually inside while True: 
-    # player.next_room()
-    # player.guess()
-
-
     while True:
         print(player.char, ", what would you like to do? Type:")
-        print("---'G' to make a guess, \n---'S' to switch rooms, \n---'M' to see the map, \n---'A' to make the final accusation, \n---'Q' to Quit.")
+        print("---'G' to make a guess \n---'S' to switch rooms \n---'M' to see the map \n---'C' to see your clues \n---'A' to make the final accusation \n---'Q' to Quit.")
         choice = input(" ").upper()
         if choice == "G":
             player.guess()
@@ -212,32 +257,16 @@ def in_play():
                     |              |              |              |
                     X--------------------------------------------X
                     """)
+        elif choice == 'C':
+            print(f'\nHere are the clues you have seen so far {seen_clues} \n')
         elif choice == "A":
             player.accusation()
+            break
         elif choice == "Q":
             break
         else:
             continue
-
-        # !!!!
-        # Need an elif for Notes to see which cards they've seen for game
-
-#CALLING THE ACTION
-random_murder()
-
-#BUILDING CARD DECKS
-cards = []
-cards.extend(characters)
-cards.extend(weapons)
-cards.extend(rooms)
-
-cards.remove(the_murder[0])
-cards.remove(the_murder[1])
-cards.remove(the_murder[2])
-
-random.shuffle(cards)
-
-#SEE CARDS
+    
 
 
 
@@ -245,14 +274,6 @@ in_play()
 
 
 
-
-# This is just messing around with my options when I get here... 
-# def guessing():
-#     if "Mrs. White" == the_murder[0]:
-#         print("Wowza")
-#     else: 
-#         print("Not this time")
-# guessing()
 
 
     
